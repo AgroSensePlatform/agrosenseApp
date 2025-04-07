fab650fa8b932c314d33d2dd64359da5efb01dde
Giorgos Kalogeitonas <gkalog2@gmail.com>
2025-04-07 10:39:50 +0300

    feat: update ShowFarm component to display farm coordinates and integrate Mapbox

--- app/components/Farm/ShowFarm.vue
+++ app/components/Farm/ShowFarm.vue
@@ -3,46 +3,55 @@
     <ActionBar title="Farm Details">
       <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="goBack" />
     </ActionBar>
-    <GridLayout rows="*" class="page__content" padding="20">
-      <StackLayout class="farm-details">
+    <GridLayout rows="*, 300" class="page__content" padding="20">
+      <StackLayout class="farm-details" row="0">
         <Label v-if="farm.name" :text="farm.name" class="farm-name" />
+
+        <!-- MapboxView -->
+        <MapboxView
+          v-if="farm.coordinates && farm.coordinates.length > 0"
+          ref="mapbox"
+          row="1"
+          height="300"
+          :accessToken="accessToken"
+          mapStyle="streets"
+          :latitude="farm.coordinates[0].lat"
+          :longitude="farm.coordinates[0].lon"
+          zoomLevel="8"
+          @mapReady="onMapReady"
+        />
+
         <Label
           v-if="farm.coordinates"
           :text="`Coordinates: ${JSON.stringify(farm.coordinates)}`"
           class="farm-coordinates"
         />
         <Label v-else text="Loading farm details..." />
-
-        <!-- <MapboxView
-          ref="mapbox"
-          height="300"
-          :accessToken="accessToken"
-          mapStyle="streets"
-          :latitude="currentLat"
-          :longitude="currentLon"
-          zoomLevel="8"
-          row="1"
-          @mapReady="onMapReady"
-        /> -->
-
-
       </StackLayout>
+
+
     </GridLayout>
   </Page>
 </template>
 
 <script>
+import { MAPBOX_ACCESS_TOKEN } from "~/shared/config";
 
 export default {
   props: {
     farm: {
       type: Object,
-    }
+      required: true,
+    },
   },
-
+  data() {
+    return {
+      accessToken: MAPBOX_ACCESS_TOKEN,
+      map: null,
+    };
+  },
   mounted() {
     console.log("ShowFarm mounted, farm:", this.farm);
-    // Directly assign the passed whole farm object to 'farm'
     if (this.farm) {
       console.log("Received farm:", this.farm);
     } else {
@@ -52,8 +61,12 @@ export default {
   methods: {
     goBack() {
       this.$navigateBack();
-    }
-  }
+    },
+    onMapReady(event) {
+      this.map = event.object;
+      console.log("Map is ready!");
+    },
+  },
 };
 </script>
 
